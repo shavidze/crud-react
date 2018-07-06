d1e7a4bc1daf37f03a626e34e51a2fe62ae96b43
Your Name <you@example.com>
2018-07-06 15:14:27 +0400

    Setup  React with Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
-import registerServiceWorker from "./registerServiceWorker";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import "./index.css";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,1 +1,6 @@
 import { combineReducers } from "redux";
+import games from "./reducers/games";
+
+export default combineReducers({
+  games
+});
